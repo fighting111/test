2af2b9e9b47dc639af55baec48be71bc957b3827
425022819 <46343786+425022819@users.noreply.github.com>
2019-01-24 17:16:47 +0800

    计科中软1801秦一博和学习代码

---
+++ 3-App组-同步文件夹/计科中软1801秦一博/学习代码/Java类基础知识/基本类型和运算符/demo.java
@@ -0,0 +1,16 @@
+public class demo {
+	
+	public static void main(String[] args) {		
+	   
+		int a = 10;
+		int b = 3;
+		int c = a+b;
+		int d = a-b;
+		int e = a/b;
+		int f = a%b;
+		System.out.println(c + "," + d + "," + e + "," + f);
+		System.out.println(a>>1);
+		System.out.println(a<<1);
+		System.out.println((5<3) || (5>3));		
+	}
+}
---
+++ 3-App组-同步文件夹/计科中软1801秦一博/学习代码/Java类基础知识/选择和循环结构/switch/demo.java
@@ -0,0 +1,17 @@
+public class demo {     
+	
+	public static void main(String[] args) {
+      
+		int a = 5;
+		int b = 1;
+		switch(a+b)
+		{
+		case 1: System.out.println("aaaaaaaaa");
+		        break;
+		case 6: System.out.println("666666666");
+		        //break;
+		default:System.out.println("7777777777");
+		}
+				
+	}
+}
