393fdfe3e6e002f65949c5a77cd33e0ec07cdf1c
137763450177 <46342055+137763450177@users.noreply.github.com>
2019-02-01 23:15:17 +0800

    第二次任务

---
+++ 1-Web组-同步文件夹/计科中软-1801江家鑫/第二次任务/Untitled-2.js
@@ -0,0 +1,17 @@
+
+
+
+window.onload=function a()
+	{	
+        var n=1;
+      
+			function lunbo()
+		{
+	if(n>2) n=1
+	div.innerHTML="<img src='a("+n+").png'/>";
+	n++;
+      };
+     var bb=setInterval(lunbo,2000);
+	}
+
+  // JavaScript Document
